c6ceeada4ee17e5fe888c2e02829acad4b3452fc
axbravo <axbravo@pucp.pe>
2015-10-27 05:11:21 -0500

    html

--- TAmoviles/www/js/index.js
+++ TAmoviles/www/js/index.js
@@ -28,7 +28,22 @@ $(document).ready(function() {
 
            var dato = arr[i].descripcion;
            var descripcion = dato.text; 
-           $("#parrafo").value=arr[i].descripcion.toString();
+           
+
+           var dato = arr[i].nombre;
+           var nombre = dato.text;
+
+           var dato = arr[i].apellidos;
+           var apellidos = dato.text;
+           
+           var dato=arr[i].marca;
+           var marca=dato.text;
+
+           var dato=arr[i].modelo;
+           var modelo=dato.text;
+
+           var dato=arr[i].asientos;
+           var asientos=dato.text;
 
            var dato = arr[i].lugarDestino;
            var lugarDestino = dato.text; 
@@ -36,11 +51,22 @@ $(document).ready(function() {
            var dato = arr[i].precio;
            var precio = dato.text; 
 
-           $("#publicaciones").append('<table><thead><tr><th></th><th></th></tr></thead><tbody><tr><td>'+arr[i].fechaPartida.toString()+'</td><td>'+arr[i].lugarEncuentro.toString()+'</td></tr><tr><td>'+arr[i].descripcion.toString()+ '</td><td>'+arr[i].lugarDestino.toString()+'</td></tr><tr><td>'+arr[i].descripcion.toString()+'</td></tr></tbody></table>');
+           var dato = arr[i].codigo;
+           var codigo = dato.text;
+
+          var dato = arr[i].correo;
+           var correo = dato.text;
+
+
+           
+           $("#publicaciones").append('<table><thead><tr><th></th><th></th></tr></thead><tbody><tr><td>'+arr[i].fechaPartida.toString()+'</td><td>'+arr[i].lugarEncuentro.toString()+'</td></tr><tr><td>'+arr[i].nombre.toString()+ '</td><td>'+arr[i].lugarDestino.toString()+'</td></tr><tr><td>'+arr[i].descripcion.toString()+'</td></tr></tbody></table>');
+          $("#mispublicaciones").append('<table data-role="table" class="ui-responsive ui-shadow" ><thead><tr><th></th><th></th></tr><tbody><tr><td>'+arr[i].fechaPartida.toString()+'</td><td>'+arr[i].lugarEncuentro.toString()+'</td></tr><tr><td>'+arr[i].nombre.toString()+'</td><td>'+arr[i].lugarDestino.toString()+'</td><tr><tr><td>'+arr[i].descripcion.toString()+'</td></tr></tbody></table><div data-role="collapsible" data-collapsed="true"><h1>Detalle</h1><p>Marca:</p><p>'+arr[i].marca.toString()+'</p><p>Modelo:</p><p>'+arr[i].modelo.toString()+'</p><p>Numero de asientos:</p><p>'+arr[i].asientos.toString()+'</p></div><br>');
+           $("#micarro").append('<label for="Marca:">Marca</label><h3>'+arr[i].marca.toString()+'</h3><br><label for="Modelo"> Modelo:</label><h3>'+arr[i].modelo.toString()+'</h3><label for="Asientos">Numero de asientos:</label><h3>'+arr[i].asientos.toString()+'</h3>');
       }
 
-      $("#publicaciones").listview('refresh');
-
+      $("#publicaciones").listview().listview('refresh');
+     /* $("#mispublicaciones").listview('refresh');
+      $("#micarro").listview('refresh');*/
     });
   
  }); 
